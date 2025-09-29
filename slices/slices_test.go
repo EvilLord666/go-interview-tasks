3c413675ef97bb2fb81ae4cccfd97b30d1f7992e
Ushakov Michale <um.nix.user@gmail.com>
2025-09-30 00:41:18 +0500

    another slice interview task

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -18,3 +18,16 @@ func TestSliceBasedOnSlicesWithoutReAlloc(t *testing.T) {
 	assert.Equal(t, slice1, []int{1, 2, 3, 6, 5, 6, 7, 8, 9, 0})
 	assert.Equal(t, slice2, []int{3, 6, 5})
 }
+
+func TestSliceBasedOnSlicesWithReAlloc(t *testing.T) {
+	// task to check what would be in a slice1 if slice2 based on slice1 item
+	// slice1 has no capacity, we are appending a new value that is leading to
+	// a new memory allocation for slice1, therefore slice1 and slice2 points to different
+	// memory addresses
+	slice1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	slice2 := slice1[2:5]
+	slice1 = append(slice1, 10)
+	slice2[0] = 9
+	assert.Equal(t, slice1, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10})
+	assert.Equal(t, slice2, []int{9, 4, 5})
+}
